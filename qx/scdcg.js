65d8ebf3df1e370cbecddc3aa9dcc451938611d1
NANXIAO1971 <33980208+NANXIAO1971@users.noreply.github.com>
2021-04-30 23:15:48 +0800

    Update scdcg.js

--- qx/scdcg.js
+++ qx/scdcg.js
@@ -26,13 +26,13 @@ https://raw.githubusercontent.com/age174/-/main/feizao.box.json
 圈X配置如下，其他软件自行测试
 [task_local]
 #食材大冲关
-10 9 * * * https://raw.githubusercontent.com/age174/-/main/scdcg.js, tag=食材大冲关, img-url=https://ae01.alicdn.com/kf/Ue353b658a4424a1e873d0b66620d06a41.jpg, enabled=true
+10 9 * * * https://raw.githubusercontent.com/NANXIAO1971/ceshi/main/qx/scdcg.js, tag=食材大冲关, img-url=https://ae01.alicdn.com/kf/Ue353b658a4424a1e873d0b66620d06a41.jpg, enabled=true
 
 [rewrite_local]
 #食材大冲关视频
-https://api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk/reward_video/reward/ url script-request-body https://raw.githubusercontent.com/age174/-/main/scdcg.js
+https://api-access.pangolin-sdk-toutiao.com/api/ad/union/sdk/reward_video/reward/ url script-request-body https://raw.githubusercontent.com/NANXIAO1971/ceshi/main/qx/scdcg.js
 #食材大冲关红包
-https://redbag.renyouwangluo.cn/api/redbag/normal url script-request-body https://raw.githubusercontent.com/age174/-/main/scdcg.js
+https://redbag.renyouwangluo.cn/api/redbag/normal url script-request-body https://raw.githubusercontent.com/NANXIAO1971/ceshi/main/qx/scdcg.js
 
 [MITM]
 hostname = redbag.renyouwangluo.cn,api-access.pangolin-sdk-toutiao.com
