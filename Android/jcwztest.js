cc52971564cf58879dfb3ccdb61b3901cc1e1da9
NANXIAO1971 <33980208+NANXIAO1971@users.noreply.github.com>
2021-08-01 23:51:41 +0800

    Update jcwztest.js

--- Android/jcwztest.js
+++ Android/jcwztest.js
@@ -72,7 +72,7 @@ const wztestheader = {
 
 function getwztestbody() {
     if ($request.url.match(/\/article\/info/)) {
-          bodyVal1 = $request.url.match(/\/article\/info.json?p=(.*?)/)
+          bodyVal1 = $request.url.match(/\/article\/info.json?p=(\w+)/)[1]
           bodyVal = 'p='+encodeURIComponent(bodyVal1)
 
         if (wztestbody) {
