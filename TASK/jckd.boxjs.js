a0bb73adbf4fb239c92531c91c7a9b9d61b94fd2
NANXIAO1971 <33980208+NANXIAO1971@users.noreply.github.com>
2021-08-07 16:24:16 +0800

    Add files via upload

---
+++ TASK/jckd.boxjs.js
@@ -0,0 +1,52 @@
+{
+    "id": "nanxiao1971.app.sub",
+    "name": "自用脚本订阅",
+    "author": "@NANXIAO1971",
+    "icon": "https://raw.githubusercontent.com/NANXIAO1971/ceshi/main/qx/nxtx.png",
+    "repo": "https://github.com/NANXIAO1971/ceshi",
+    "apps": [
+      {
+      "author": "@shaolin-kongfu", 
+      "keys": [
+        "lookStartbody", 
+        "lookbody"
+      ], 
+      "repo": "https://github.com/shaolin-kongfu/js_scripts/blob/main/jckkz.js", 
+      "id": "jcKkz", 
+      "script": "https://raw.githubusercontent.com/shaolin-kongfu/js_scripts/main/jckkz.js", 
+      "name": "晶彩看看赚", 
+      "icons": [ 
+        "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png",
+        "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png"  
+      ]
+    },
+    {
+      "author": "@shaolin-kongfu", 
+      "keys": [
+        "wzbody"
+      ], 
+      "repo": "https://github.com/shaolin-kongfu/js_scripts/blob/main/jcwz.js", 
+      "id": "jcRead", 
+      "script": "https://raw.githubusercontent.com/shaolin-kongfu/js_scripts/main/jcwz.js", 
+      "name": "晶彩自动阅读", 
+      "icons": [ 
+        "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png",
+        "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png"
+      ]
+    },
+    {
+      "author": "@shaolin-kongfu", 
+      "keys": [
+        "qdbody"
+      ], 
+      "repo": "https://github.com/shaolin-kongfu/js_scripts/blob/main/jcqd.js", 
+      "id": "jcSign", 
+      "script": "https://raw.githubusercontent.com/shaolin-kongfu/js_scripts/main/jcqd.js", 
+      "name": "晶彩签到", 
+      "icons": [ 
+        "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png",
+        "https://cdn.jsdelivr.net/gh/NANXIAO1971/ceshi@main/picture/jckd1.png"
+      ]
+    }         
+    ]
+}
